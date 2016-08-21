5e614096bd8283d0ad2e31ac65ba5cc009ba7ad6
Vandergard <vadim.terentev.26s@gmail.com>
2016-08-21 12:22:19 +0300

    In, Out, Authenication

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Myapp::Application.routes.draw do
 
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
-  match '/signin',  to: 'sessions#destroy',     via: 'delete'
+  match '/signout',  to: 'sessions#destroy',     via: 'delete'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
